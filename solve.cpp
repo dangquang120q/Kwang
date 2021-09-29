66632839b39535100cc194eae7db435f7582d6c4
dangquang120q <90206681+dangquang120q@users.noreply.github.com>
2021-09-30 05:06:33 +0700

    Add files via upload

--- solve.cpp
+++ solve.cpp
@@ -1,24 +1,17 @@
-#include<stdio.h>
-int main(){
-	int T;
-	scanf("%d",&T);
-	while(T--){
-		int k;
-		int a[100];
-		scanf("%d",&k);
-		for(int i = 0;i<k;i++){
-			scanf("%d",&a[i]);
-		}
-		int b;
-		for(int i = 0;i<k - 1;i++){
-			for(int j = i + 1;j<k;j++){
-				if(a[i] > a[j]) {
-					b = a[i];
-					a[i] = a[j];
-					a[j] = b;
-				}
-			}
-		}
-		printf("%d\n",a[k - 1]+a[k - 2]+a[k - 3]);
-	}
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--){
+       LL r, b, d;
+       cin >> r >> b >> d;
+       if (r > b) swap(r, b);
+       if(r + r * d >= b) cout << "YES";
+       else cout << "NO";
+       cout << endl;
+    }
+    return 0;
 }
